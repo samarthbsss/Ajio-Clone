92727cb3848fa1dce26f46b545511c89194b1762
anannyaSaikia <anannyasaikia1998@gmail.com>
2023-03-09 14:13:37 +0530

    first commit homepage

---
+++ footer.js
@@ -0,0 +1,78 @@
+let footer = `<div id="main-footer">
+        <div id="footer-top">
+            <img src="Icons/icon-1.png"
+                alt="Easy Exchange">
+            <img src="Icons/icon-2.png"
+                alt="Hand Picked">
+            <img src="Icons/icon-3.png"
+                alt="Assured Quality">
+        </div>
+        <div id="footer-content">
+            <div id="upper">
+                <div id="u1">
+                    <div class="headline">Ajio</div>
+                    <p>Who We Are</p>
+                    <p>Join Our Team</p>
+                    <p>Terms & Conditions</p>
+                    <p>We Respect Your Privacy</p>
+                    <p>Fees & Payments</p>
+                    <p>Returns & Refunds Policy</p>
+                    <p>Promotions Terms & Conditions</p>
+                </div>
+                <div id="u2">
+                    <div class="headline">Help</div>
+                    <p>Track Your Order</p>
+                    <p>Frequently Asked Questions</p>
+                    <p>Returns</p>
+                    <p>Cancellations</p>
+                    <p>Payments</p>
+                    <p>Customer Care</p>
+                    <p>How Do I Return My Coupon</p>
+                </div>
+                <div id="u3">
+                    <div class="headline">Shop By</div>
+                    <p>Men</p>
+                    <p>Women</p>
+                    <p>Kids</p>
+                    <p>Indie</p>
+                    <p>Stores</p>
+                    <p>New Arrivals</p>
+                    <p>Brand Directory</p>
+                    <p>Home</p>
+                    <p>Collections</p>
+                </div>
+                <div id="u4">
+                    <div class="headline">Follow us</div>
+                    <p>
+                        <a href="https://www.facebook.com/AJIOlife">Facebook</a>
+                    </p>
+                    <p>
+                        <a href="https://www.instagram.com/AJIOlife/">Instagram-AJIOlife</a>
+                    </p>
+                    <p>
+                        <a href="https://www.instagram.com/ajioluxe/">Instagram-AJIO LUXE</a>
+                    </p>
+                    <p>
+                        <a href="https://twitter.com/AJIOLife">Twitter</a>
+                    </p>
+                    <p>
+                        <a href="https://www.pinterest.com/ajiolife/">Pinterest</a>
+                    </p>
+                </div>
+
+            </div>
+            <hr>
+            <div id="lower">
+                <div id="left">
+                    <div class="headline">Payment methods</div>
+                    <img src="Icons/icon-4.png"
+                        alt="Payment Methods">
+                </div>
+                <div id="right">
+                    <div class="headline">Secure systems</div>
+                    <img src="Icons/icon-5.png" alt="Encription">
+                </div>
+            </div>
+        </div>
+    </div>`;
+document.getElementById("main-footer").innerHTML = footer;
